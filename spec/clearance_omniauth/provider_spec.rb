b11351b06b3a42cdc29b0be9cf76ae0e3ffe67ce
Dmitry Zuev <mail@dzuev.ru>
2016-07-09 08:20:44 +0300

    Add more tests to Provider

--- spec/clearance_omniauth/provider_spec.rb
+++ spec/clearance_omniauth/provider_spec.rb
@@ -6,4 +6,20 @@ describe ClearanceOmniauth::Provider do
     expect(provider.provider).to eq(:developer)
     expect(provider.params).to eq([])
   end
+
+  it 'initializes with provider and params' do
+    provider = ClearanceOmniauth::Provider.new(provider: :developer, params: %w(one two))
+    expect(provider.provider).to eq(:developer)
+    expect(provider.params).to eq(%w(one two))
+  end
+
+  it 'fails to initialize without provider' do
+    expect { ClearanceOmniauth::Provider.new }.to raise_error(ArgumentError)
+  end
+
+  it 'can not write to provider or params directly' do
+    provider = ClearanceOmniauth::Provider.new(provider: :developer)
+    expect { provider.provider = :facebook }.to raise_error(NoMethodError)
+    expect { provider.params = %w(one two) }.to raise_error(NoMethodError)
+  end
 end
